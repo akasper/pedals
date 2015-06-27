012ea5e57355487462d4d98f0c163586ce40dc8f
Andrew Kasper <andrew.m.kasper@gmail.com>
2015-06-26 20:59:50 -0400

    Garbagefucker wireframe implementation, up do (but not including) checkout and cart

--- app/controllers/pedals_controller.rb
+++ app/controllers/pedals_controller.rb
@@ -1,6 +1,15 @@
 class PedalsController < ApplicationController
+  autocomplete :pedal, :name, full: true
+
   def index
-    @pedals = Pedal.all
-    @sale   = Sale.new
+    @pedal = Pedal.new
+  end
+
+  def create
+    @pedal = Pedal.find_by_name(params[:pedal][:name])
+  end
+
+  def sell
+    @pedal = Pedal.find_by_name(params[:id])
   end
 end
--- app/models/pedal.rb
+++ app/models/pedal.rb
@@ -1,18 +1,18 @@
 class Pedal < ActiveRecord::Base
-  before_create :set_alt_id
+  # before_create :set_alt_id
 
   validates :name, presence: true
   validates :name, length: { minimum:2, maximum:255 }
 
-  validates :alt_id, uniqueness: true
-
+  # validates :alt_id, uniqueness: true
+  #
   validates :min_price, presence: true
   validates :min_price, numericality: { greater_than: 0.0}
 
   validates :max_price, presence: true
   validates :max_price, numericality: { greater_than: 0.0}
 
-  def set_alt_id
-    self.alt_id ||= self.name.downcase.gsub(/\s+/, '-')
-  end
+  # def set_alt_id
+  #   self.alt_id ||= self.name.downcase.gsub(/\s+/, '-')
+  # end
 end
